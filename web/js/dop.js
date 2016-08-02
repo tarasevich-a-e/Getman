5a88dda3369f66f8014aca604a9083642cb2effb
tarasevich-a-e <tarasevich-a-e@gmail.com>
2016-08-02 18:59:02 +0600

    полностью доделан GUI. Начата реализация отправка формы с input'ами.

--- web/js/dop.js
+++ web/js/dop.js
@@ -1,10 +1,22 @@
     var ind=0;
+    var ind_input=0;
+    var ind_headers=0;
+    var flag_zagolovka=0;//1-Input;2-Headers;3-Body
+    var mas_params = [];
+    var ind_mas_params = 0;
+    var mas_input = [];
+    var ind_mas_input = 0;
+    var mas_headers = [];
+    var ind_mas_headers = 0;
+    var mas_body = "";
+
     var vstavka = document.getElementById("input_block");
     var in_name = [];
     var in_value = [];
     var new_in_id = [];
     /*New version*/
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Function add params in to path
     function AddParamN(){
 
@@ -16,6 +28,8 @@
     var div_param_path = document.createElement('div');
             div_param_path.className = "ch2_param_path_1";
             div_param_path.id = "h2_param_path_" + ind_tec;
+            mas_params[ind_mas_params] = ind_tec;
+            ind_mas_params++;
 
     ////////////////////////////////////////////////////////
     //Create div - class = cdpath_param_key
@@ -70,6 +84,7 @@
     ind++;
     }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Function delete params in to path
     function DeleteParam(id_el) {
 
@@ -82,12 +97,14 @@
         var t = vstavka_div_param.style.height;
         t = t.substr(0,t.length -2);
         vstavka_div_param.style.height =  (t - 50) + 'px';
-    ind--;
+    mas_params[mas_params.indexOf(id_el)] = -1;
+    //ind--;
     }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     //Function select razdel (Input/Headers/Body)
     function CheckZagolovok(id_zagolovka) {
-
+        flag_zagolovka = id_zagolovka;
         var videl_zagolovok = document.getElementById("ih3_zagolovki_" + id_zagolovka);
 
         videl_zagolovok.style.backgroundColor = 'rgb(70,70,70)';
@@ -113,7 +130,7 @@
             vidimost_razdela.style.display = 'none';
         } else {
             var vidimost_razdela = document.getElementById("h4_zag_headers");
-            vidimost_razdela.style.display = 'flex';
+            vidimost_razdela.style.display = 'inline-block';
         }
         if(id_zagolovka != 3) {
             var videl_zagolovok_n = document.getElementById("ih3_zagolovki_" + 3);
@@ -129,7 +146,358 @@
 
     }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Function add new input in to blok "Input"
+    function AddNewInput() {
+        var ind_input_tec = ind_input;
+        var vstavka_div_inp = document.getElementById("dh4_zag_input_2");
 
+        ////////////////////////////////////////////////////////
+        //Create div - class = cdh4_zag_input_20
+        var div_input = document.createElement('div');
+            div_input.id = "dh4_zag_input_2" + ind_input_tec;
+            div_input.className = "cdh4_zag_input_20";
+            mas_input[ind_mas_input] = ind_input_tec;
+            ind_mas_input++;
+
+
+            //Button delete block
+            var div_inp_zag_1 = document.createElement('div');
+                div_inp_zag_1.id = "dh4_zag_input_11" + ind_input_tec;
+                div_inp_zag_1.className = "cdh4_zag_input_11";
+
+                var inp_zag_1 = document.createElement('input');
+                    inp_zag_1.id = "ih4_zag_input_11" + ind_input_tec;
+                    inp_zag_1.className = "cih4_zag_input_11";
+                    inp_zag_1.setAttribute('val_inp',ind_input_tec);
+                    inp_zag_1.setAttribute('type',"button");
+                    inp_zag_1.setAttribute('value',"-");
+                    inp_zag_1.setAttribute('onclick',"DeleteInput(" + ind_input_tec + ")");
+
+            div_inp_zag_1.appendChild(inp_zag_1);
+
+            //Text "Input"
+            var div_inp_zag_1_1 = document.createElement('div');
+                div_inp_zag_1_1.id = "dh4_zag_input_14" + ind_input_tec;
+                div_inp_zag_1_1.style.width = "280px";
+                div_inp_zag_1_1.style.textAlign = "center";
+                div_inp_zag_1_1.className = "cdh4_zag_input_1ob_text";
+
+                var div_inp_zag_11_1 = document.createElement('div');
+                    div_inp_zag_11_1.className = "inp_zag";
+
+                    var span_zag_11 = document.createElement('span');
+                        span_zag_11.innerHTML = 'INPUT:';
+
+                div_inp_zag_11_1.appendChild(span_zag_11);
+
+            div_inp_zag_1_1.appendChild(div_inp_zag_11_1);
+
+            //Text "Name"
+            var div_inp_zag_2 = document.createElement('div');
+                div_inp_zag_2.id = "dh4_zag_input_14" + ind_input_tec;
+                div_inp_zag_2.className = "cdh4_zag_input_1ob_text";
+
+                var div_inp_zag_21 = document.createElement('div');
+                    div_inp_zag_21.className = "inp_zag";
+
+                    var span_zag_2 = document.createElement('span');
+                        span_zag_2.innerHTML = 'Name:';
+
+                div_inp_zag_21.appendChild(span_zag_2);
+
+            div_inp_zag_2.appendChild(div_inp_zag_21);
+
+            //Input "Name"
+            var div_inp_zag_3 = document.createElement('div');
+                div_inp_zag_3.id = "dh4_zag_input_15" + ind_input_tec;
+                div_inp_zag_3.className = "cdh4_zag_input_1ob_inp";
+
+                var inp_zag_31 = document.createElement('input');
+                    inp_zag_31.id = "ih4_zag_input_15" + ind_input_tec;
+                    inp_zag_31.className = "cdh4_zag_input_1ob_si";
+
+            div_inp_zag_3.appendChild(inp_zag_31);
+
+            //Text "Value"
+            var div_inp_zag_4 = document.createElement('div');
+                div_inp_zag_4.id = "dh4_zag_input_16" + ind_input_tec;
+                div_inp_zag_4.className = "cdh4_zag_input_1ob_text";
+
+                var div_inp_zag_41 = document.createElement('div');
+                    div_inp_zag_41.className = "inp_zag";
+
+                    var span_zag_4 = document.createElement('span');
+                        span_zag_4.innerHTML = 'Value:';
+
+                div_inp_zag_41.appendChild(span_zag_4);
+
+            div_inp_zag_4.appendChild(div_inp_zag_41);
+
+            //Input "Value"
+            var div_inp_zag_5 = document.createElement('div');
+                div_inp_zag_5.id = "dh4_zag_input_17" + ind_input_tec;
+                div_inp_zag_5.className = "cdh4_zag_input_1ob_inp";
+
+                var inp_zag_51 = document.createElement('input');
+                    inp_zag_51.id = "ih4_zag_input_17" + ind_input_tec;
+                    inp_zag_51.className = "cdh4_zag_input_1ob_si";
+
+            div_inp_zag_5.appendChild(inp_zag_51);
+
+            //Text "Type"
+            var div_inp_zag_6 = document.createElement('div');
+                div_inp_zag_6.id = "dh4_zag_input_17" + ind_input_tec;
+                div_inp_zag_6.className = "cdh4_zag_input_1ob_text";
+
+                var div_inp_zag_61 = document.createElement('div');
+                    div_inp_zag_61.className = "inp_zag";
+
+                    var span_zag_6 = document.createElement('span');
+                        span_zag_6.innerHTML = 'Type:';
+
+                div_inp_zag_61.appendChild(span_zag_6);
+
+            div_inp_zag_6.appendChild(div_inp_zag_61);
+
+            //Input "Type"
+            var div_inp_zag_7 = document.createElement('div');
+                div_inp_zag_7.id = "dh4_zag_input_18" + ind_input_tec;
+                div_inp_zag_7.className = "cdh4_zag_input_1ob_inp";
+
+                var inp_zag_71 = document.createElement('input');
+                    inp_zag_71.id = "ih4_zag_input_18" + ind_input_tec;
+                    inp_zag_71.className = "cdh4_zag_input_1ob_si";
+                    var sel_opt = document.getElementById("sdh4_zag_input_13").value;
+                    if(sel_opt == "file"){
+                        inp_zag_51.setAttribute('type',"file");
+                        inp_zag_51.style.width = '160px';
+                        }
+                    inp_zag_71.value = sel_opt;
+                    inp_zag_71.setAttribute('disabled',"disabled");
+
+            div_inp_zag_7.appendChild(inp_zag_71);
+
+        div_input.appendChild(div_inp_zag_1);
+        div_input.appendChild(div_inp_zag_1_1);
+        div_input.appendChild(div_inp_zag_2);
+        div_input.appendChild(div_inp_zag_3);
+        div_input.appendChild(div_inp_zag_4);
+        div_input.appendChild(div_inp_zag_5);
+        div_input.appendChild(div_inp_zag_6);
+        div_input.appendChild(div_inp_zag_7);
+
+        vstavka_div_inp.appendChild(div_input);
+    ind_input++;
+    }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Function delete new input in to blok "Input"
+    function DeleteInput(id_el) {
+        var id_del_el = document.getElementById("dh4_zag_input_2" + id_el);
+
+            id_del_el.parentNode.removeChild(id_del_el);
+
+        mas_input[mas_input.indexOf(id_el)] = -1;
+        //ind_input--;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Function add headers
+    function AddHeaders() {
+        var ind_headers_tec = ind_headers;
+        var vstavka_div_param = document.getElementById("dh4_zag_headers_2");
+
+        ////////////////////////////////////////////////////////
+        //Create div - class = ch2_param_path_1
+        var div_param_path = document.createElement('div');
+                div_param_path.className = "ch2_param_path_1";
+                div_param_path.id = "h2_param_header_" + ind_headers_tec;
+
+        ////////////////////////////////////////////////////////
+        //Create div - class = cdpath_param_key
+        var div_param_path_1 = document.createElement('div');
+                div_param_path_1.className = "cdpath_param_key";
+                div_param_path_1.id = "cdpk_header_" + ind_headers_tec;
+
+        //Create div - class = cdpath_param_value
+        var div_param_path_2 = document.createElement('div');
+                div_param_path_2.className = "cdpath_param_value";
+                div_param_path_2.id = "cdpv_header_" + ind_headers_tec;
+
+        //Create div - class = cdpath_param_del
+        var div_param_path_3 = document.createElement('div');
+                div_param_path_3.className = "cdpath_header_del";
+                div_param_path_3.id = "cdpd_header_" + ind_headers_tec;
+
+        ////////////////////////////////////////////////////////
+        //Create input - class = cipath_param_key
+        var input_param_path_1 = document.createElement('input');
+                input_param_path_1.className = "cipath_param_key";
+                input_param_path_1.id = "ipath_header_key_" + ind_headers_tec;
+                input_param_path_1.setAttribute('type',"text");
+                input_param_path_1.setAttribute('placeholder',"key");
+                div_param_path_1.appendChild(input_param_path_1);
+
+        //Create input - class = cipath_param_value
+        var input_param_path_2 = document.createElement('input');
+                input_param_path_2.className = "cipath_param_value";
+                input_param_path_2.id = "ipath_header_value_" + ind_headers_tec;
+                input_param_path_2.setAttribute('type',"text");
+                input_param_path_2.setAttribute('placeholder',"value");
+                div_param_path_2.appendChild(input_param_path_2);
+
+        //Create input - class = cipath_param_del
+        var input_param_path_3 = document.createElement('input');
+                input_param_path_3.className = "cipath_param_del";
+                input_param_path_3.id = "ipath_header_del_" + ind_headers_tec;
+                input_param_path_3.setAttribute('type',"button");
+                input_param_path_3.setAttribute('vd',ind_headers_tec);
+                input_param_path_3.setAttribute('onclick',"DeleteHeader(" + ind_headers_tec + ")");
+                input_param_path_3.value = "x";
+                div_param_path_3.appendChild(input_param_path_3);
+
+        div_param_path.appendChild(div_param_path_1);
+        div_param_path.appendChild(div_param_path_2);
+        div_param_path.appendChild(div_param_path_3);
+
+        vstavka_div_param.style.height = (50 + ind_headers_tec * 50) + 'px';
+        vstavka_div_param.appendChild(div_param_path);
+
+        ind_headers++;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Function delete params in to path
+        function DeleteHeader(id_el) {
+
+            var id_del_el = document.getElementById("h2_param_header_" + id_el);
+
+            id_del_el.parentNode.removeChild(id_del_el);
+
+            //Reduce height div
+            var vstavka_div_param = document.getElementById("dh4_zag_headers_2");
+            var t = vstavka_div_param.style.height;
+            t = t.substr(0,t.length -2);
+            vstavka_div_param.style.height =  (t - 50) + 'px';
+        //ind_headers--;
+        }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Function send request to server
+    function SendRequest() {
+        var flag_er = false;
+        var ind_create_input = 0;
+        var str = document.getElementById("iurls").value;
+
+        if(flag_zagolovka == 1) {
+        //Send INPUT
+
+        //Create input and set attributes(name, value, type, form) - massiv inputov (mas_input)
+
+            if(mas_input.length != 0) {
+                //if set "form" and "action"
+                if((document.getElementById("ih4_zag_input_15").value != "") && (document.getElementById("ih4_zag_input_17").value != "")){
+                    //If form: name, action not null
+
+                    var vstavka_inputov = document.getElementById("inviz_block1");
+                    var form_name = document.getElementById("ih4_zag_input_15").value;
+                    var form_action = document.getElementById("ih4_zag_input_17").value;
+                    //Form send to url "action"
+                    document.getElementById("iurld").value = form_action;
+                    var form_method = document.getElementById("isel_method").value;
+                    var form_id = "myForm";
+
+                    var new_form;
+                    //Create form
+                    if(document.getElementById("myForm") == null) {
+
+                        //Create new
+                        new_form = document.createElement('form');
+                        new_form.id = form_id;
+                        new_form.name = form_name;
+                        new_form.setAttribute('action', form_action);
+                        new_form.setAttribute('method', form_method);
+                        vstavka_inputov.appendChild(new_form);
+
+                    } else {
+
+                        //Update form
+                        new_form = document.getElementById("myForm");
+                        new_form.name = form_name;
+                        new_form.setAttribute('action', form_action);
+                        new_form.setAttribute('method', form_method);
+
+                    }
+
+                    for(var i = 0; i < mas_input.length; i++) {
+                        //If input name not null
+                        if((mas_input[i] != -1) && (document.getElementById("ih4_zag_input_15" + mas_input[i]).value != "")) {
+
+                            var new_input_v;
+                            //Create input
+                            if(document.getElementById("new_in_" + ind_create_input) == null) {
+                                new_input_v = document.createElement('input');
+                                new_input_v.id = "new_in_" + ind_create_input;
+                                new_input_v.className = "cinviz";
+                                new_input_v.name = document.getElementById("ih4_zag_input_15" + mas_input[i]).value;
+                                new_input_v.value = document.getElementById("ih4_zag_input_17" + mas_input[i]).value;
+
+                                new_input_v.setAttribute('type', document.getElementById("ih4_zag_input_18" + mas_input[i]).value);
+                                new_input_v.setAttribute('form', form_id);
+                                vstavka_inputov.appendChild(new_input_v);
+                            } else {
+                                new_input_v = document.getElementById("new_in_" + ind_create_input);
+                                new_input_v.name = document.getElementById("ih4_zag_input_15" + mas_input[i]).value;
+                                new_input_v.value = document.getElementById("ih4_zag_input_17" + mas_input[i]).value;
+                                new_input_v.setAttribute('form', form_id);
+                            }
+                            ind_create_input++;
+
+                        } else {
+                            if(mas_input[i] != -1){
+                                alert('Заполните поле "name" у Input!');
+                                flag_er = true;
+                                }
+                        }
+                    }
+
+
+                } else {
+                    alert('Заполните поля "name" и "action" у Form!');
+                    flag_er = true;
+                }
+            }
+        }
+
+        if(flag_zagolovka == 2) {
+        //Send Headers
+
+        }
+
+        if(flag_zagolovka == 3) {
+        //Send Body
+
+        }
+
+        str = str + document.getElementById("iurld").value;
+        if(mas_params.length != 0) {
+                //We have params_path
+                    for(var i = 0; i < mas_params.length; i++) {
+                        if((mas_params[i] != -1) && (document.getElementById("ipath_param_key_" + mas_params[i]).value != "")) {
+                            str = str + '?' + document.getElementById("ipath_param_key_" + mas_params[i]).value + '=' + document.getElementById("ipath_param_value_" + mas_params[i]).value + '&';
+                        }
+                    }
+                }
+
+        if(flag_er == false) {
+            document.getElementById("myForm").submit();
+            alert('Запрос отправлен!\n' + str);
+        } else {
+            alert('Ошибка, запрос не отправлен!\n' + str);
+        }
+    return false;
+    }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     /*Old version*/
     function AddInput(){
         var ind_tec = ind;
